8e9448f9feb2f1a418801bcb8745678f6297266f
Matvei <matvesch@protonmail.com>
2023-08-07 11:35:03 +0300

    initial commit

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,5 @@
+import styles from './Logo.module.scss';
+
+export function Logo() {
+  return <div className={styles.logo}></div>;
+}
---
+++ src/components/sections/Header/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.scss';
+
+export function Header() {
+  return (
+    <section className={styles.header}>
+      <h1>Header</h1>
+    </section>
+  );
+}
