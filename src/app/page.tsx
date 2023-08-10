1eb609b3d5cf65675e15beb4ec3eeeba436bb515
Tommy Lidkovsky <armkudr@gmail.com>
2023-08-10 19:46:59 +0300

    spin anim added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@sections/Header/Header';
 import { HeroAnimation } from '@/components/hero-animation/HeroAnimaton';
+import { SpinAnimation } from '@/components/spin-animation/SpinAnimation';
 import styles from '@styles/page.module.scss';
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className={styles.main}>
       <Header />
       <HeroAnimation />
+      <SpinAnimation />
       <h1>main page</h1>
     </main>
   );
