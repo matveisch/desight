2eb4ed193f1208fe8fe6463a45f525a88c3e6e74
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-08-19 17:14:42 +0300

    fix image attempts

--- src/sections/HeroImage/HeroImage.tsx
+++ src/sections/HeroImage/HeroImage.tsx
@@ -7,11 +7,13 @@ import right from '@images/positiveflow.png';
 export function HeroImage() {
   return (
     <section className={styles.heroimage}>
+      <div className={styles.border}>
         <h2>От идеи к реальности. Пиксель за пикселем</h2>
-       <div className={styles.image}>
-        <Image src={left} alt='left' className={styles.left} />
-        <Image src={right} alt='right' className={styles.right}/>
-       </div>
+        <div className={styles.image}>
+          <Image src={left} alt='left' className={styles.left} />
+          <Image src={right} alt='right' className={styles.right}/>
+        </div>
+      </div>   
     </section>
   );
 }
