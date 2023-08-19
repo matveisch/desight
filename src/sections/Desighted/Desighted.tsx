4ea7685f19238307a504d53bb97a527b562b536a
Tommy Lidkovsky <armkudr@gmail.com>
2023-08-19 19:02:23 +0300

    desighted in process

--- src/sections/Desighted/Desighted.tsx
+++ src/sections/Desighted/Desighted.tsx
@@ -4,40 +4,60 @@ import { motion } from 'framer-motion';
 import React, { useRef, useState, useEffect } from 'react';
 
 export default function Desighted() {
-  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const containerRef = useRef(null);
+  const buttonRef = useRef(null);
 
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const [isInBounders, setInBounders] = useState<boolean>(false);
   const [buttonSize, setButtonSize] = useState({
     width: 1,
     height: 1,
   });
-
-  const [mousePosition, setMousePosition] = useState({
-    x: null,
-    y: null,
+  const [containerSize, setContainerSize] = useState({
+    width: 1000,
+    height: 570,
   });
 
-  const constraintsRef = useRef(null);
-  const buttonRef = useRef(null);
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  //   useEffect(() => {
+  //     if (
+  //       mousePosition.x > buttonSize.width / 2 &&
+  //       mousePosition.x < containerSize.width - buttonSize.width / 2 &&
+  //       mousePosition.y > buttonSize.height / 2 &&
+  //       mousePosition.y < containerSize.height - buttonSize.height / 2
+  //     ) {
+  //       setInBounders(true);
+  //     } else {
+  //       setInBounders(false);
+  //     }
+  //   }, [mousePosition]);
 
   useEffect(() => {
     setButtonSize({
       width: buttonRef.current?.clientWidth,
       height: buttonRef.current?.clientHeight,
     });
-    console.log(buttonSize);
-  }, [buttonRef]);
+    setContainerSize({
+      width: containerRef.current.clientWidth,
+      height: containerRef.current.clientHeight,
+    });
+    console.log(containerSize);
+  }, []);
 
   useEffect(() => {
     const updateButtonPosition = (ev: object) => {
       setMousePosition({
-        x: ev.screenX - constraintsRef.current?.offsetLeft,
-        y: ev.pageY - constraintsRef.current?.offsetTop,
+        x: ev.screenX - containerRef.current?.offsetLeft,
+        y: ev.pageY - containerRef.current?.offsetTop,
       });
     };
-
     isHovered
       ? window.addEventListener('mousemove', updateButtonPosition)
       : window.removeEventListener('mousemove', updateButtonPosition);
+
     return () => {
       window.removeEventListener('mousemove', updateButtonPosition);
     };
@@ -47,22 +67,39 @@ export default function Desighted() {
     <>
       <motion.div
         className={styles.main}
-        ref={constraintsRef}
+        ref={containerRef}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         <motion.button
-          //   initial={{
-          //     left: 0,
-          //     top: 0,
-          //     right: 0,
-          //     bottom: 0,
-          //   }}
           //   animate={buttonPosition}
-          dragConstraints={constraintsRef}
+
+          //   animate={{
+          //     left: isHovered
+          //       ? mousePosition.x > buttonSize.width / 2 &&
+          //         mousePosition.x < containerSize.width - buttonSize.width / 2
+          //         ? mousePosition.x - buttonSize.width / 2
+          //         : mousePosition.x >= containerSize.width - buttonSize.width / 2
+          //         ? containerSize.width - buttonSize.width / 2
+          //         : mousePosition <= buttonSize.width / 2
+          //         ? 0
+          //         : 'unset'
+          //       : 'unset',
+
+          //     top: isHovered
+          //       ? mousePosition.y > buttonSize.height / 2 &&
+          //         mousePosition.y < containerSize.height - buttonSize.height / 2
+          //         ? mousePosition.y - buttonSize.height / 2
+          //         : mousePosition > buttonSize.height / 2 &&
+          //           mousePosition.y >=
+          //             containerSize.height - buttonSize.height / 2
+          //         ? containerSize.height - buttonSize.height / 2
+          //         : 0
+          //       : 'unset',
+          //   }}
           animate={{
-            left: mousePosition.x - buttonSize.width / 2,
-            top: mousePosition.y - buttonSize.height / 2,
+            left: isHovered ? mousePosition.x - buttonSize.width / 2 : 'unset',
+            top: isHovered ? mousePosition.y - buttonSize.height / 2 : 'unset',
           }}
           transition={{ type: 'tween' }}
           className={styles.button}
