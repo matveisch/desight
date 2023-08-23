bf6969bc0da620dde4df06559cca9e524d21d6da
Matvei <matvesch@protonmail.com>
2023-08-23 21:40:50 +0300

    "Updated CSS rules and components structure for responsiveness"

--- src/sections/CompleteSolution/CompleteSolution.tsx
+++ src/sections/CompleteSolution/CompleteSolution.tsx
@@ -1,10 +1,14 @@
 import styles from './CompleteSolution.module.scss';
 import CompleteDescription from '@components/CompleteDescription/CompleteDescription';
+import { SpinAnimation } from '@components/spinAnimation/SpinAnimation';
 
 export default function CompleteSolution() {
   return (
     <div className={styles.completeSolution}>
-      <CompleteDescription />
+      <div className={styles.header}>
+        <CompleteDescription />
+        <SpinAnimation />
+      </div>
     </div>
   );
 }
