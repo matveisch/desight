adb375653d51885e65959630f756899701e10eac
Tommy Lidkovsky <armkudr@gmail.com>
2023-08-25 20:41:55 +0300

    complete section aimation finished mb

--- src/components/CompleteDescription/CompleteDescription.tsx
+++ src/components/CompleteDescription/CompleteDescription.tsx
@@ -1,10 +1,25 @@
+'use client';
+
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
 import Image from 'next/image';
 import styles from './CompleteDescription.module.scss';
 import arrow from '@images/arrow-link.svg';
 
 export default function CompleteDescription() {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, {
+    margin: '0px 100px -300px 0px',
+    once: true,
+  });
   return (
-    <div className={styles.completeDescription}>
+    <motion.div
+      initial={{ x: -40, opacity: 0 }}
+      animate={isInView ? { x: 0, opacity: 1 } : {}}
+      transition={{ duration: 0.8, type: 'tween', ease: 'easeOut' }}
+      className={styles.completeDescription}
+      ref={ref}
+    >
       <h3>From $999</h3>
       <h2>Complete Web Solution</h2>
       <p className={styles.text}>
@@ -16,6 +31,6 @@ export default function CompleteDescription() {
         <p>Узнать подробности</p>
         <Image src={arrow} alt={'arrow'} />
       </button>
-    </div>
+    </motion.div>
   );
 }
