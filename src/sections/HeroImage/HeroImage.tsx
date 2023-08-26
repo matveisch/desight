d70e7ca91342a0df27d0a38f2e0acda9f4b321c6
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2023-08-26 20:34:45 +0300

    stream is scalable now

--- src/sections/HeroImage/HeroImage.tsx
+++ src/sections/HeroImage/HeroImage.tsx
@@ -11,8 +11,12 @@ export function HeroImage() {
         <h2>От идеи к реальности. Пиксель за пикселем</h2>
       </div>   
       <div className={styles.image}>
-          <Image src={left} alt='left' className={styles.left} />
-          <Image src={right} alt='right' className={styles.right}/>
+        <div className={styles.leftContainer}>
+            <Image src={left} alt='left' className={styles.left}  />
+          </div>
+        <div className={styles.rightContainer}>
+          <Image src={right} alt='right' className={styles.right}  />
+        </div>
         </div>
     </section>
   );
