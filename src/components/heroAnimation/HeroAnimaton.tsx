5283a8a2928513b880ad8c45afa5f6294f4eaffa
Matvei <matvesch@protonmail.com>
2023-09-17 22:10:51 +0300

    Refactor HeroAnimation component to optimize code
    Updated HeroAnimation component to improve code readability and maintainability. Replaced null initial animation values with actual numeric initial states, for clarity regarding animation transitions. Removed nested motion.div elements for each icon and replaced them with a map function, DRYing up the code substantially and making future functionality updates easier. These changes do not affect the functionality of the component.

--- src/components/heroAnimation/HeroAnimaton.tsx
+++ src/components/heroAnimation/HeroAnimaton.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useState, useEffect } from 'react';
 import styles from './HeroAnimation.module.scss';
 import { motion } from 'framer-motion';
@@ -6,7 +7,7 @@ import Image from 'next/image';
 import cyrsqIcon from '@images/cyrsq-icon.svg';
 import devIcon from '@images/dev-icon.svg';
 import penIcon from '@images/pen-icon.svg';
-import setIcon from '@images/setting-icon.svg';
+// import setIcon from '@images/setting-icon.svg';
 import squareIcon from '@images/squares-icon.svg';
 import userIcon from '@images/user-icon.svg';
 import clickText from '@images/clickandhold.svg';
@@ -14,42 +15,44 @@ import Globus from '@images/Globus.svg';
 
 const iconsVariants = {
   tapped: {
-    rotate: [null, 300],
-    opacity: [null, 1, 1, 0],
+    rotate: [0, 300],
+    opacity: [1, 1, 0],
     transition: {
       rotate: { times: [0, 1], duration: 2, ease: 'easeIn' },
       opacity: { times: [0, 0.4, 0.99, 1], duration: 2 },
     },
   },
   notTapped: {
-    rotate: [null, 0, 0, 0],
-    opacity: [null, 0],
+    rotate: [0, 0, 0, 0],
+    opacity: [1, 0],
     transition: {
       rotate: { times: [0, 0.1, 0.6, 1], duration: 0.1 },
       opacity: { times: [0, 1], duration: 0.2 },
     },
   },
 };
+
 const iconVariants = {
   tapped: {
-    y: [null, '-200%', '-200%', '0%', '-400%'],
-    rotate: [null, 200],
+    y: ['0%', '-200%', '-200%', '0%', '-400%'], // Initial value replaced with '0%'
+    rotate: [0, 200], // Initial value replaced with '0'
     transition: {
       y: { times: [0, 0.4, 0.7, 0.85, 1], duration: 2 },
       rotate: { times: [0, 1], duration: 2 },
     },
   },
   notTapped: {
-    y: [null, 0, 0],
+    y: ['0%', 0, 0], // Initial value replaced with '0%'
     rotate: 0,
     transition: { y: { times: [0, 0.5, 1], duration: 0.4 } },
   },
 };
+
 const globeVariants = {
   tapped: {
-    scale: [null, 0.4, 1],
-    rotate: [null, -200, 0],
-    opacity: [null, 1, 0],
+    scale: [1, 0.4, 1], // Initial value replaced with '1'
+    rotate: [0, -200, 0], // Initial value replaced with '0'
+    opacity: [1, 1, 0], // Initial value replaced with '1'
     filter: 'blur(0px)',
     transition: {
       scale: { times: [0, 0.85, 1], duration: 2 },
@@ -67,8 +70,8 @@ const globeVariants = {
     },
   },
   notTapped: {
-    scale: [null, 1],
-    rotate: [null, 360],
+    scale: [1, 1], // Initial value replaced with '1'
+    rotate: [0, 360], // Initial value replaced with '0'
     opacity: 1,
     filter: 'blur(5px)',
     transition: {
@@ -81,6 +84,7 @@ const globeVariants = {
 export function HeroAnimation() {
   const [isHovered, setHovered] = useState<boolean>(false);
   const [isTapped, setIsTapped] = useState<boolean>(false);
+  const icons = [cyrsqIcon, penIcon, devIcon, userIcon, squareIcon];
   const handleMouseUp = () => {
     setIsTapped(false);
   };
@@ -102,36 +106,15 @@ export function HeroAnimation() {
   //   }
   // }, [isTapped]);
   return (
-    <motion.div
-      animate={isTapped ? 'tapped' : 'notTapped'}
-      className={styles.main}
-    >
+    <motion.div animate={isTapped ? 'tapped' : 'notTapped'} className={styles.main}>
       <motion.div variants={iconsVariants} className={styles.icons}>
-        <motion.div className={styles.iconWrapper}>
-          <motion.div variants={iconVariants} className={styles.icon}>
-            <Image src={cyrsqIcon} alt="test" />
+        {icons.map((icon, index) => (
+          <motion.div key={index} className={styles.iconWrapper}>
+            <motion.div variants={iconVariants} className={styles.icon}>
+              <Image src={icon} alt={`icon ${index}`} />
+            </motion.div>
           </motion.div>
-        </motion.div>
-        <motion.div className={styles.iconWrapper}>
-          <motion.div variants={iconVariants} className={styles.icon}>
-            <Image src={penIcon} alt="test" />
-          </motion.div>
-        </motion.div>
-        <motion.div className={styles.iconWrapper}>
-          <motion.div variants={iconVariants} className={styles.icon}>
-            <Image src={devIcon} alt="test" />
-          </motion.div>
-        </motion.div>
-        <motion.div className={styles.iconWrapper}>
-          <motion.div variants={iconVariants} className={styles.icon}>
-            <Image src={userIcon} alt="test" />
-          </motion.div>
-        </motion.div>
-        <motion.div className={styles.iconWrapper}>
-          <motion.div variants={iconVariants} className={styles.icon}>
-            <Image src={squareIcon} alt="test" />
-          </motion.div>
-        </motion.div>
+        ))}
       </motion.div>
 
       <motion.div
