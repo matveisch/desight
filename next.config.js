c7eb88e450b9d1e5fa510577bba5ff7c80649765
Matvei <matvesch@protonmail.com>
2023-09-27 17:51:17 +0300

    Add redirect rule in Next.js configuration
    This commit introduces a redirect rule in the Next.js configuration file (next.config.js). Now, users navigating to the root URL ("/") will be automatically redirected to the "/en" path. The change ensures a unified and consistent user experience across the site by directing all users to the English version of the site by default.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [{ source: '/', destination: '/en', permanent: true }];
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: ['./src'],
     prependData: `@import "~@styles/variables.scss";`,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
